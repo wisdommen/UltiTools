2023c96bbb97fedeec94c764859ce3c78039b074
wisdommen <wisdommen@users.noreply.github.com>
2020-09-06 22:13:22 +1000

    9.6更新，版本3.0.0 1.修复了聊天栏输入"%"会报错的问题

--- src/main/java/com/minecraft/ultikits/listener/ChatListener.java
+++ src/main/java/com/minecraft/ultikits/listener/ChatListener.java
@@ -16,7 +16,7 @@ public class ChatListener implements Listener {
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         String prefixes_str = UltiTools.getInstance().getConfig().getString("chat_prefix");
-        //UnknownFormatConversionException: Conversion = '%'
-        event.setFormat(Objects.requireNonNull(PlaceholderAPI.setPlaceholders(player, prefixes_str))+ ChatColor.WHITE+event.getMessage());
+        event.setMessage(event.getMessage());
+        event.setFormat(Objects.requireNonNull(PlaceholderAPI.setPlaceholders(player, prefixes_str))+ChatColor.WHITE+" %2$s");
     }
 }
