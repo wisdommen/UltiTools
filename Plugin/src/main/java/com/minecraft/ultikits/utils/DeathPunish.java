49972c24f34b4684c03b36353481e6c0435edade
qianmo2233 <qianmo2233@users.noreply.github.com>
2020-10-18 15:48:33 +0800

    10.18更新 版本3.4.0
    10.18更新 版本3.4.0
    1.添加了死亡惩罚的处理类

---
+++ Plugin/src/main/java/com/minecraft/ultikits/utils/DeathPunish.java
@@ -0,0 +1,38 @@
+package com.minecraft.ultikits.utils;
+
+import com.minecraft.ultikits.ultitools.UltiTools;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
+
+public class DeathPunish {
+    public static void takeMoney(Player player, int money) {
+        EconomyUtils.withdraw(player, Math.min(EconomyUtils.checkMoney(player), money));
+    }
+
+    public static void Exec(String cmd) {
+        UltiTools.getInstance().getServer().dispatchCommand(UltiTools.getInstance().getServer().getConsoleSender(), cmd);
+    }
+
+    public static void Exec(Player player, String cmd) {
+        UltiTools.getInstance().getServer().dispatchCommand(player, cmd);
+    }
+
+    public static void takeItem(Player player, int times) {
+        Random random = new Random();
+        Inventory inventory = player.getInventory();
+        int count = 1;
+        if (inventory.getContents().length != 0) {
+            while (count < times) {
+                ItemStack itemStack = player.getInventory().getItem(random.nextInt(45));
+                if (itemStack != null) {
+                    itemStack.setAmount(random.nextInt(itemStack.getAmount()));
+                    inventory.remove(itemStack);
+                    count++;
+                }
+            }
+        }
+    }
+}
