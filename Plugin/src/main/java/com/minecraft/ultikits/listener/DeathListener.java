8462ab03d00823c0d37327a3381b362f68b19b93
qianmo2233 <qianmo2233@users.noreply.github.com>
2020-10-24 16:30:32 +0800

    10.24更新,版本3.3.4
    1.完成了死亡惩罚功能

--- Plugin/src/main/java/com/minecraft/ultikits/listener/DeathListener.java
+++ Plugin/src/main/java/com/minecraft/ultikits/listener/DeathListener.java
@@ -3,24 +3,63 @@ package com.minecraft.ultikits.listener;
 import com.minecraft.ultikits.config.ConfigController;
 import com.minecraft.ultikits.ultitools.UltiTools;
 import com.minecraft.ultikits.utils.DeathPunishUtils;
+import com.minecraft.ultikits.utils.LanguageUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+import java.util.List;
 
 public class DeathListener implements Listener {
+
+    Player player;
+    List<String> list;
+    String world;
+
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        Player player = event.getEntity().getPlayer();
-        if (isEnabled()) {
-            if(isEnableItemDrop()) {
-                DeathPunishUtils.takeItem(player, 1);
+    public void onEntityDamage(EntityDamageEvent event) {
+
+        if (event.getEntity() instanceof Player) {
+            player = (Player) event.getEntity();
+            world = player.getWorld().getName();
+        } else {
+            return;
+        }
+
+        if (player.getHealth() >= event.getFinalDamage()) {
+            return;
+        }
+
+        if (isEnableItemDrop()) {
+            list = getWorldsEnabledItemDrop();
+            for (String s : list) {
+                if (s.equals(world)) {
+                    DeathPunishUtils.takeItem(player, getItemDrop());
+                    player.sendMessage(ChatColor.RED + UltiTools.languageUtils.getWords("punish_item_dropped"));
+                }
             }
         }
-    }
 
-    private boolean isEnabled() {
-        return UltiTools.getInstance().getConfig().getBoolean("death_punish");
+        if (isEnableMoneyDrop()) {
+            list = getWorldsEnabledMoneyDrop();
+            for (String s : list) {
+                if (s.equals(world)) {
+                    DeathPunishUtils.takeMoney(player, getMoneyDrop());
+                    player.sendMessage(ChatColor.RED + UltiTools.languageUtils.getWords("punish_money_dropped"));
+                }
+            }
+        }
+
+        if (isEnablePunishCommands()) {
+            list = getWorldsEnabledPunishCommand();
+            for (String s : list) {
+                if (s.equals(world)) {
+                    DeathPunishUtils.Exec(getPunishCommands(), player.getName());
+                }
+            }
+        }
     }
 
     private boolean isEnableMoneyDrop() {
@@ -31,7 +70,31 @@ public class DeathListener implements Listener {
         return (boolean) ConfigController.getValue("enable_punish_commands");
     }
 
-    public boolean isEnableItemDrop() {
+    private boolean isEnableItemDrop() {
         return (boolean) ConfigController.getValue("enable_item_drop");
+    }
+
+    private int getMoneyDrop() {
+        return (int) ConfigController.getValue("money_dropped_ondeath");
+    }
+
+    private int getItemDrop() {
+        return (int) ConfigController.getValue("item_dropped_ondeath");
+    }
+
+    private List getPunishCommands() {
+        return (List) ConfigController.getValue("punish_command");
+    }
+
+    private List getWorldsEnabledItemDrop() {
+        return (List) ConfigController.getValue("worlds_enabled_item_drop");
+    }
+
+    private List getWorldsEnabledMoneyDrop() {
+        return (List) ConfigController.getValue("worlds_enabled_money_drop");
+    }
+
+    private List getWorldsEnabledPunishCommand() {
+        return (List) ConfigController.getValue("worlds_enabled_punish_commands");
     }
 }
--- Plugin/src/main/java/com/minecraft/ultikits/utils/DeathPunishUtils.java
+++ Plugin/src/main/java/com/minecraft/ultikits/utils/DeathPunishUtils.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
 import java.util.Random;
 
 public class DeathPunishUtils {
@@ -12,12 +13,12 @@ public class DeathPunishUtils {
         EconomyUtils.withdraw(player, Math.min(EconomyUtils.checkMoney(player), money));
     }
 
-    public static void Exec(String cmd) {
-        UltiTools.getInstance().getServer().dispatchCommand(UltiTools.getInstance().getServer().getConsoleSender(), cmd);
-    }
-
-    public static void Exec(Player player, String cmd) {
-        UltiTools.getInstance().getServer().dispatchCommand(player, cmd);
+    public static void Exec(List<String> cmd, String playerName) {
+        for (String s : cmd) {
+            UltiTools.getInstance().getServer().dispatchCommand(
+                    UltiTools.getInstance().getServer().getConsoleSender(), s.replace("{PLAYER}", playerName)
+            );
+        }
     }
 
     public static void takeItem(Player player, int drop) {
