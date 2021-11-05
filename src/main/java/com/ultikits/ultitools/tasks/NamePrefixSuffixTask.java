80a41a31f4a3ca5303d2cd435d4df7e36fb84677
wisdommen <41609381+wisdommen@users.noreply.github.com>
2021-11-05 14:43:51 +1100

    11.5 版本5.1.4 1.修复右键发送交易申请时报错的问题 2.增加了自定义指令别称功能(游戏内可补全) 3.增加了/email tools 邮箱操作GUI页面 4.移除了/email tool 指令 5.增加了自定义指令别称功能(游戏内可补全) 6.增加了背包备份功能 7.修复了查看玩家背包指令的文字显示问题 8.新增重制版ban功能(ultiban,ultibanip,ultibanlist指令) 9.增加了随机传送的权限ultikits.tools.command.wild

--- src/main/java/com/ultikits/ultitools/tasks/NamePrefixSuffixTask.java
+++ src/main/java/com/ultikits/ultitools/tasks/NamePrefixSuffixTask.java
@@ -16,17 +16,26 @@ public class NamePrefixSuffixTask extends BukkitRunnable {
         for (Player player : Bukkit.getOnlinePlayers()) {
             String prefix;
             String suffix;
-            if (UltiTools.isPAPILoaded && UltiTools.getInstance().getConfig().getBoolean("enable_PAPI") ){
+            if (UltiTools.isPAPILoaded && UltiTools.getInstance().getConfig().getBoolean("enable_PAPI")) {
                 try {
                     prefix = Objects.requireNonNull(PlaceholderAPI.setPlaceholders(player, UltiTools.getInstance().getConfig().getString("name_prefix")));
                     suffix = Objects.requireNonNull(PlaceholderAPI.setPlaceholders(player, UltiTools.getInstance().getConfig().getString("name_suffix")));
-                }catch (Exception e){
+                } catch (Exception e) {
                     prefix = "";
                     suffix = "";
                 }
             } else {
                 prefix = "";
                 suffix = "";
+            }
+            String version = UltiTools.getInstance().getServer().getVersion();
+            if (Integer.parseInt(version.split("MC: ")[1].replaceAll("[.)]", "")) < 1130) {
+                if (prefix.length() > 16){
+                    prefix = prefix.substring(0, 16);
+                }
+                if (suffix.length() > 16) {
+                    suffix = suffix.substring(0, 16);
+                }
             }
             TitlesUtils.setPrefixSuffix(player, prefix, suffix);
         }
