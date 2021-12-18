cabbdccfc1274954bb5c40fa794713aa89aae6b1
“your_uqianmo2233 <67282958@qq.com>
2021-12-18 17:28:52 +0800

    12.12 版本5.1.5 添加了正版玩家可跳过GUI登录的功能

--- src/main/java/com/ultikits/ultitools/checker/OnlineModePlayerChecker.java
+++ src/main/java/com/ultikits/ultitools/checker/OnlineModePlayerChecker.java
@@ -1,15 +1,20 @@
 package com.ultikits.ultitools.checker;
 
-import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class OnlineModePlayerChecker {
 
     public static boolean isOnlineMode(String PlayerName) {
         try {
-            URL url = new URL("https://api.mojang.com/users/profiles/minecraft/oyo123" + PlayerName);
-        } catch (MalformedURLException e) {
+            URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + PlayerName);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            if (connection.getResponseCode() == 200) return true;
+            if (connection.getResponseCode() == 204) return false;
+        } catch (Exception e) {
             e.printStackTrace();
+            return false;
         }
         return false;
     }
